7b386cf87bd453174072e50cf6e47dddd13f16d1
Tom de Wit <ttomdewit@gmail.com>
2015-12-04 21:29:25 +0100

    Create gulpfile, templates for .scss files

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+/**
+ * Main Gulp file
+ *
+ * This main Gulp file imports all modules and
+ * creates all Gulp tasks accordingly.
+ *
+ * Table of contents
+ * 1) Load plugins
+ * 2) Styles
+ * 3) Modernizr
+ * 4) Scripts
+ * 5) Images
+ * 6) SVG
+ * 7) Watch
+ */
+
+
+
+/**
+ * 1) Load plugins
+ *
+ * Require all modules and define variables.
+ */
+
+ var gulp = require('gulp'),
+     sass = require('gulp-sass'),
+     autoprefixer = require('gulp-autoprefixer'),
+     minifycss = require('gulp-minify-css'),
+     jshint = require('gulp-jshint'),
+     uglify = require('gulp-uglify'),
+     imagemin = require('gulp-imagemin'),
+     rename = require('gulp-rename'),
+     concat = require('gulp-concat'),
+     notify = require('gulp-notify'),
+     cache = require('gulp-cache'),
+     livereload = require('gulp-livereload'),
+     del = require('del');
+
+
+
+/**
+ * 2) Styles
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
+
+
+
+/**
+ * 3) Modernizr
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
+
+
+
+/**
+ * 4) Scripts
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
+
+
+
+/**
+ * 5) Images
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
+
+
+
+/**
+ * 6) SVG
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
+
+
+
+/**
+ * 7) Watch
+ *
+ * [[ Explain what this Gulp task does ]].
+ */
